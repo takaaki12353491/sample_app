dde84bcf799e8dcd4ad26c8dd133ea8398fa5b34
takaaki <takaaki12353491@gmail.com>
2019-01-11 19:35:59 +0000

    use S3 bucket

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -2,6 +2,12 @@ class PictureUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   process resize_to_limit: [400, 400]
   
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
+  
   storage :file
 
   # アップロードファイルの保存先ディレクトリは上書き可能
--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,4 +1,3 @@
-=begin
 if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
@@ -11,4 +10,3 @@ if Rails.env.production?
     config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
-=end
